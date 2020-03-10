bedc23d790100ab28af80213d6a8e231a319de5b
vonubat <vonubat@gmail.com>
2020-03-10 23:58:22 +0300

    Покажите знак числа

---
+++ Conditions/signNumber.js
@@ -0,0 +1,9 @@
+'use strict';
+let number=prompt('Напиште любое целое число (положительное или отрицательное)', '');
+if (number>0) {
+  alert('1');
+} else if (number<0) {
+  alert('-1');
+} else {
+  alert('0');
+}
