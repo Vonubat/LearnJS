eae7589af45b0aead1b3259af892f6d26c006a79
vonubat <vonubat@gmail.com>
2020-03-10 23:48:12 +0300

    Название JavaScript
    Используя конструкцию if..else, напишите код, который будет спрашивать: „Какое «официальное» название JavaScript?“

---
+++ Conditions/ECMAScript.js
@@ -0,0 +1,7 @@
+'use strist';
+let answer = prompt ('Какое официальное название JavaScript?', '');
+if (answer=="ECMAScript") {
+  alert (' Верно!');
+} else {
+  alert (' Не знаете? ECMAScript!');
+}
