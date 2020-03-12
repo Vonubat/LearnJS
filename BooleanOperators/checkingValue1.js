684f0d59fe4507c481ee2466ed4ab851d272b6fe
vonubat <vonubat@gmail.com>
2020-03-12 21:35:06 +0300

    Проверка значения из диапазона

---
+++ BooleanOperators/checkingValue1.js
@@ -0,0 +1,8 @@
+'use strict';
+// Напишите условие if для проверки, что переменная age1 находится в диапазоне между 14 и 90 включительно.
+let age1=prompt('What is your age1?', '');
+if (age1>=14 && age1<=90) {
+  alert ('Verification successful');
+} else {
+  alert ('Verification is failed');
+} 
