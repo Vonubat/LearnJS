dab8154dd2faa15619c0c57454d2cafdf4f74904
vonubat <vonubat@gmail.com>
2020-03-14 19:06:21 +0300

    Create rewriteCheckAge.js
    Перепишите функцию, используя оператор '?' или '||'

---
+++ Functions/rewriteCheckAge.js
@@ -0,0 +1,29 @@
+'use strict';
+/*
+Следующая функция возвращает true, если параметр age больше 18.
+
+В ином случае она задаёт вопрос confirm и возвращает его результат.
+
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    return confirm('Родители разрешили?');
+  }
+}
+Перепишите функцию, чтобы она делала то же самое, но без if, в одну строку.
+
+Сделайте два варианта функции checkAge:
+
+Используя оператор ?
+Используя оператор ||
+*/
+function checkAgeByQuestion(age) {
+  return (age>18)?true:confirm('Did parents allow?');
+};
+function checkAgeByOr(age) {
+  return (age>18) || confirm('Did parents allow?'); 
+}
+let age=prompt ('How old are you?','');
+checkAgeByQuestion(age);
+checkAgeByOr(age);
