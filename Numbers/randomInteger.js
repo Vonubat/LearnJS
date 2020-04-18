a95d9c8f37db52342d6a8aa4b6e271f6687b4008
vonubat <vonubat@gmail.com>
2020-04-18 18:46:22 +0300

    Create randomInteger.js
    учайное целое число от min до max

---
+++ Numbers/randomInteger.js
@@ -0,0 +1,14 @@
+'use strict';
+/*
+Напишите функцию randomInteger(min, max), которая генерирует случайное целое (integer) число от min до max (включительно).
+
+Любое число из интервала min..max должно появляться с одинаковой вероятностью.
+*/
+
+function randomInteger(min, max) {
+  // случайное число от min до (max+1)
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+
+alert(randomInteger(1, 3));
