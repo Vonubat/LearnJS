2a95d2928231342eff7972c2b0a5a0125fe23c42
vonubat <vonubat@gmail.com>
2020-05-27 22:41:55 +0300

    Create getAverageAge.js
    Получить средний возраст

---
+++ ArrayMethods/getAverageAge.js
@@ -0,0 +1,27 @@
+'use strict';
+/*
+Напишите функцию getAverageAge(users), которая принимает массив объектов со свойством age и возвращает средний возраст.
+
+Формула вычисления среднего арифметического значения: (age1 + age2 + ... + ageN) / N.
+
+Например:
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 29 };
+let arr = [ vasya, petya, masha ];
+
+alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
+*/
+
+function getAverageAge(arr) {
+  let result = arr.reduce((sum, next) => (sum + next.age), 0) / (arr.length);
+  return result;
+}
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 29 };
+let arr = [vasya, petya, masha];
+
+alert(getAverageAge(arr));
