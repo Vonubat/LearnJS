48bcfc3273db779138ae9ad962c70061ef6b69d1
vonubat <vonubat@gmail.com>
2020-05-31 20:07:13 +0300

    Create range.js
    Cделать объект range итерируемым

---
+++ Iterators/range.js
@@ -0,0 +1,23 @@
+'use strict';
+let range = {
+  from: 1,
+  to: 5,
+
+  [Symbol.iterator]() {
+    this.current = this.from;
+    this.last = this.to;
+    return this;
+  },
+
+  next() {
+    if (this.current <= this.last) {
+      return { done: false, value: this.current++ };
+    } else {
+      return { done: true };
+    }
+  }
+};
+
+for (let num of range) {
+  alert(num); // 1, затем 2, 3, 4, 5
+}
