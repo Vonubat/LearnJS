b8b47fead99f3399c908b2e438f3e1846426103b
vonubat <vonubat@gmail.com>
2020-06-17 23:46:48 +0300

    Create topSalary.js
    Максимальная зарплата

---
+++ DestructiveAssignment/topSalary.js
@@ -0,0 +1,39 @@
+'use strict';
+/*
+У нас есть объект salaries с зарплатами:
+
+let salaries = {
+  "John": 100,
+  "Pete": 300,
+  "Mary": 250
+};
+Создайте функцию topSalary(salaries), которая возвращает имя самого высокооплачиваемого сотрудника.
+
+Если объект salaries пустой, то нужно вернуть null.
+Если несколько высокооплачиваемых сотрудников, можно вернуть любого из них.
+P.S. Используйте Object.entries и деструктурирование, чтобы перебрать пары ключ/значение.
+*/
+
+let salaries = {
+  "John": 100,
+  "Pete": 300,
+  "Mary": 250,
+  "4len": 400
+};
+
+function topSalary(salaries) {
+
+  let max = 0;
+  let maxName = null;
+
+  for (let [name, salary] of Object.entries(salaries)) {
+    if (max < salary) {
+      max = salary;
+      maxName = name;
+    }
+  }
+
+  return maxName;
+}
+
+alert(topSalary(salaries));
