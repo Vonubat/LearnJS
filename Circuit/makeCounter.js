bbfaf7bb3c9b71fdc2c39c9cf29630e94a19eda6
vonubat <vonubat@gmail.com>
2020-07-16 23:06:29 +0300

    Create makeCounter.js
    Функция счётчик

---
+++ Circuit/makeCounter.js
@@ -0,0 +1,15 @@
+'use strict';
+/* Пример */
+function makeCounter() {
+  let count = 0;
+  return function () {
+    return count++;
+  };
+}
+
+let counter1 = makeCounter();
+let counter2 = makeCounter();
+
+alert(counter1()); // 0
+alert(counter1()); // 1
+alert(counter2()); // 0 (независимо)
